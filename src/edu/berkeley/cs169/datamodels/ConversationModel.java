3b7c9e0f2d11b15abb899359d90dda913e8049e0
edmondmct@gmail.com <edmondmct@gmail.com>
2011-10-14 20:45:22 -0700

    Add all the DataModel

--- src/edu/berkeley/cs169/datamodels/ConversationModel.java
+++ src/edu/berkeley/cs169/datamodels/ConversationModel.java
@@ -1,5 +1,7 @@
 package edu.berkeley.cs169.datamodels;
 
+import java.util.List;
+
 public class ConversationModel {
 	ContactModel other;
 	List<MessageModel> messages;
--- src/edu/berkeley/cs169/datamodels/MorseCodeModel.java
+++ src/edu/berkeley/cs169/datamodels/MorseCodeModel.java
@@ -1,5 +1,7 @@
 package edu.berkeley.cs169.datamodels;
 
+import java.util.List;
+
 public class MorseCodeModel {
 	List<Integer> rawData; 
 }
