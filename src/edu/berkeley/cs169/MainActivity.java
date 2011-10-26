5e523234ad670804cdd695d8a1a551c1642dd323
Mark Wei <markwei@gmail.com>
2011-10-26 02:21:22 -0700

    Vibrate shortcode for each Activity. No screen orientation. Write key capture framework for menu navigation. Separate out component which can be used by all Activities

--- src/edu/berkeley/cs169/MainActivity.java
+++ src/edu/berkeley/cs169/MainActivity.java
@@ -1,27 +1,105 @@
 package edu.berkeley.cs169;
 
-import edu.berkeley.cs169.utils.BMUtils;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import edu.berkeley.cs169.utils.BMUtils;
 
 public class MainActivity extends Activity {
+
+	// {{ Copy and edit these fields and methods for each Activity
+
+	// Edit for each Activity
 	public static final String TAG = "MainActivity";
+	// Same for all Activities
+	public boolean upPressed = false;
+	public boolean downPressed = false;
+
+	// Same for all Activities
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		vibrateShortCode();
+	}
+
+	// Edit for each Activity
+	protected void vibrateShortCode() {
+		String alert = getResources().getString(R.string.main_shortcode);
+
+		BMUtils.textToVibration(alert, this);
+	}
+
+	// Edit for each Activity
+	protected void vibrateHelp() {
+		String alert = getResources().getString(R.string.main_help);
+
+		BMUtils.textToVibration(alert, this);
+	}
+
+	// Edit for each Activity
+	protected void startUpAction() {
+		Log.d(TAG, "Clicked UP");
+		BMUtils.textToVibration("e", this);
+	}
+
+	// Edit for each Activity
+	protected void startDownAction() {
+		Log.d(TAG, "Clicked DOWN");
+		BMUtils.textToVibration("t", this);
+	}
+
+	// Same for all Activities
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_VOLUME_UP:
+			upPressed = true;
+			return true;
+		case KeyEvent.KEYCODE_VOLUME_DOWN:
+			downPressed = true;
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	// Same for all Activities
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_VOLUME_UP:
+		case KeyEvent.KEYCODE_VOLUME_DOWN:
+			if (upPressed && downPressed) {
+				vibrateHelp();
+			} else if (upPressed) {
+				startUpAction();
+			} else if (downPressed) {
+				startDownAction();
+			}
+			upPressed = false;
+			downPressed = false;
+			return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+
+	// }}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		LinearLayout layoutUp = (LinearLayout) findViewById(R.id.layout_up);
 		layoutUp.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Log.d(TAG, "Clicked UP");
+				startUpAction();
 			}
 		});
 
@@ -30,17 +108,8 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				Log.d(TAG, "Clicked DOWN");
+				startDownAction();
 			}
 		});
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-		String alert = getResources().getString(R.string.main_alert);
-		
-		BMUtils.textToVibration(alert, this);
 	}
 }
