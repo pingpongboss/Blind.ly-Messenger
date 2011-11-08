d54f70c58785b16c1b1f26dec5b16a30baa7e168
Mark Wei <markwei@gmail.com>
2011-11-07 22:33:32 -0800

    power doesn't work so make help long press both up and down, then make 'power' action just tapping both up and down

--- src/edu/berkeley/cs169/utils/NavigationKeyInterpreter.java
+++ src/edu/berkeley/cs169/utils/NavigationKeyInterpreter.java
@@ -3,10 +3,12 @@ package edu.berkeley.cs169.utils;
 import android.view.KeyEvent;
 
 public class NavigationKeyInterpreter {
+	public static long HOLD_THRESHOLD = 3 * Utils.INPUT_SPEED_BASE;
+
 	NavigationKeyInterpreterResultListener listener;
 
-	boolean upPressed = false;
-	boolean downPressed = false;
+	long upKeyDownTimestamp = -1;
+	long downKeyDownTimestamp = -1;
 
 	public NavigationKeyInterpreter(
 			NavigationKeyInterpreterResultListener listener) {
@@ -16,28 +18,36 @@ public class NavigationKeyInterpreter {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_VOLUME_UP:
-			upPressed = true;
+			if (upKeyDownTimestamp == -1)
+				upKeyDownTimestamp = event.getEventTime();
 			return true;
 		case KeyEvent.KEYCODE_VOLUME_DOWN:
-			downPressed = true;
+			if (downKeyDownTimestamp == -1)
+				downKeyDownTimestamp = event.getEventTime();
 			return true;
 		}
 		return false;
 	}
 
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		long upDt = event.getEventTime() - upKeyDownTimestamp;
+		long downDt = event.getEventTime() - downKeyDownTimestamp;
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_VOLUME_UP:
 		case KeyEvent.KEYCODE_VOLUME_DOWN:
-			if (upPressed && downPressed) {
-				listener.onKeyInterpreterResult(2);
-			} else if (upPressed) {
-				listener.onKeyInterpreterResult(0);
-			} else if (downPressed) {
-				listener.onKeyInterpreterResult(1);
+			if (upKeyDownTimestamp != -1 && downKeyDownTimestamp != -1) {
+				if (upDt > HOLD_THRESHOLD && downDt > HOLD_THRESHOLD) {
+					listener.onKeyInterpreterResult(NavigationKeyInterpreterResultListener.UP_AND_DOWN_HOLD);
+				} else {
+					listener.onKeyInterpreterResult(NavigationKeyInterpreterResultListener.UP_AND_DOWN);
+				}
+			} else if (upKeyDownTimestamp != -1) {
+				listener.onKeyInterpreterResult(NavigationKeyInterpreterResultListener.UP);
+			} else if (downKeyDownTimestamp != -1) {
+				listener.onKeyInterpreterResult(NavigationKeyInterpreterResultListener.DOWN);
 			}
-			upPressed = false;
-			downPressed = false;
+			upKeyDownTimestamp = -1;
+			downKeyDownTimestamp = -1;
 			return true;
 		}
 		return false;
@@ -47,6 +57,7 @@ public class NavigationKeyInterpreter {
 		public static final int UP = 0;
 		public static final int DOWN = 1;
 		public static final int UP_AND_DOWN = 2;
+		public static final int UP_AND_DOWN_HOLD = 3;
 
 		public void onKeyInterpreterResult(int resultCode);
 
