97132de892ceedbae20a706b5be852c5fc8e0d29
Jesse Chen <github@jessechen.net>
2011-11-20 03:14:27 -0800

    disable touch events on recipientinputactivity functional

--- src/edu/berkeley/cs169/adapter/ContactCursorAdapter.java
+++ src/edu/berkeley/cs169/adapter/ContactCursorAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
