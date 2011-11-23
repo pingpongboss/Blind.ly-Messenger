4ac7a179c8d3f3b765438da3072f4191b0633124
Mark Wei <markwei@gmail.com>
2011-11-22 22:17:36 -0800

    comment MainPreferenceActivity.java

--- src/edu/berkeley/cs169/activity/MainPreferenceActivity.java
+++ src/edu/berkeley/cs169/activity/MainPreferenceActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import edu.berkeley.cs169.R;
 
+//preference screen
 public class MainPreferenceActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		// preference menu is defined in XML
 		addPreferencesFromResource(R.xml.preference);
 	}
 }
